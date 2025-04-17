cf9f794eb23ffa406863a55a7c32b2e9946d0744
Astranika <mymail@example.com>
2025-04-17 14:20:41 +0300

    исправление

--- Table1/app/database.py
+++ Table1/app/database.py
@@ -2,6 +2,8 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from sqlalchemy import text
+import subprocess
+import logging
 from sqlmodel import SQLModel
 import os
 import time
@@ -9,16 +11,24 @@ from sqlalchemy.exc import OperationalError
 from dotenv import load_dotenv
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
-
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+def apply_migrations():
+    try:
+        subprocess.run(["alembic", "upgrade", "head"], check=True)
+        logger.info("Alembic миграции успешно применены")
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Ошибка применения миграций Alembic: {e}")
+        raise
 
 def init_db():
     retries = 10
     while retries > 0:
         try:
-            SQLModel.metadata.create_all(engine)
-            print("Успешное подключение к БД")
+            apply_migrations()
+            print("Успешное подключение к БД + успешное применение миграций Alembic")
             break
         except OperationalError:
             retries -= 1
@@ -27,3 +37,6 @@ def init_db():
             if retries == 0:
                 print("Не удалось подключиться к БД")
                 raise
+        except Exception as other_error:
+            logger.critical(f"Неизвестная ошибка при подключении к БД: {other_error}")
+            raise
